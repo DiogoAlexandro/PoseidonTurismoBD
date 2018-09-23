1e03f2bbee6ea9d23191dfb0de51b503dc1f8721
DiogoAlexandro <dodo.alexandro@gmail.com>
2018-09-23 15:43:49 -0300

    PoseidonTurismoBD
    Banco de dados do cruzeiros maritimo

---
+++ PoseidonTurismoBD.sql
@@ -0,0 +1,5 @@
+create database PoseidonTurismoBD;
+use PoseidonTurismoBD;
+
+Create table Cruzeiro (id int primary key not null,
+clientes varchar(20) not null, navios varchar(20) not null, rotas varchar(20), datas varchar (10),pacotes varchar(20));
