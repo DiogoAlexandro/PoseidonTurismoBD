11ee79a1d6794b2b35d76e03207e776ff83ba25c
DiogoAlexandro <dodo.alexandro@gmail.com>
2018-09-23 15:50:18 -0300

    Update PoseidonTurismoBD.sql

--- PoseidonTurismoBD.sql
+++ PoseidonTurismoBD.sql
@@ -1,5 +1,5 @@
 create database PoseidonTurismoBD;
 use PoseidonTurismoBD;
 
-Create table Cruzeiro (id int primary key not null,
+Create table Cruzeiro (id int primary key not null auto_increment,
 clientes varchar(20) not null, navios varchar(20) not null, rotas varchar(20), datas varchar (10),pacotes varchar(20));
