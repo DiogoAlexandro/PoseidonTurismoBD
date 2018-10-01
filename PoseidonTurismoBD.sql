6659c197ceca23dc70cbe560aaf958cc370e2f96
Diogo Alexandro <dodo.alexandro@gmail.com>
2018-09-30 22:33:53 -0300

    Adicionando a tabela clientes
    Criação da tabela clientes e da foreign key relacionada aos clientes

--- PoseidonTurismoBD.sql
+++ PoseidonTurismoBD.sql
@@ -1,5 +1,7 @@
 create database PoseidonTurismoBD;
 use PoseidonTurismoBD;
 
-Create table PoseidonTurismo (id int primary key not null auto_increment,
-clientes varchar(20) not null, navios varchar(20) not null, rotas varchar(20), datas varchar (10),pacotes varchar(20));
+Create table PoseidonTurismo (id int primary key not null auto_increment, CONSTRAINT fk_Clientes FOREIGN KEY (id) REFERENCES 
+clientes (id_clientes), clientes varchar(20) not null, navios varchar(20) not null, rotas varchar(20), datas varchar (10),pacotes varchar(20));
+Create table Clientes ( id_clientes int primary key auto_increment not null, nome varchar(50), endereco varchar (50), telefone varchar (40),
+ observacao varchar (100));
