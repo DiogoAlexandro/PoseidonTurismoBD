20615d170bc2664c7398ac70808f8d3b6f4ed3e3
Diogo Alexandro <dodo.alexandro@gmail.com>
2018-10-09 15:39:49 -0300

    Patch 1.2
    Alteração na forma dos campos e tabelas.
    Criação de novas tabelas para receber os campos do programa

--- PoseidonTurismoBD.sql
+++ PoseidonTurismoBD.sql
@@ -1,7 +1,90 @@
 create database PoseidonTurismoBD;
 use PoseidonTurismoBD;
 
-Create table PoseidonTurismo (id int primary key not null auto_increment, CONSTRAINT fk_Clientes FOREIGN KEY (id) REFERENCES 
-clientes (id_clientes), clientes varchar(20) not null, navios varchar(20) not null, rotas varchar(20), datas varchar (10),pacotes varchar(20));
-Create table Clientes ( id_clientes int primary key auto_increment not null, nome varchar(50), endereco varchar (50), telefone varchar (40),
- observacao varchar (100));
+CREATE TABLE `bkp` (
+  `mes_bkp` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE `clientes` (
+  `telefone` varchar(9) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `bairro` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`telefone`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE `forma_pgto` (
+  `id_pgto` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_pgto`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE `navios` (
+  `id_navios` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_navios` varchar(50) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id_navios`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE `pacotes` (
+  `id_pacotes` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao_pac` varchar(20) DEFAULT NULL,
+  `valorpadrao` decimal(10,0) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id_pacotes`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(15) DEFAULT NULL,
+  `senha` varchar(50) DEFAULT NULL,
+  `tipo` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE `valorpadrao` (
+  `valor` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE `vendas` (
+  `id_venda` int(11) NOT NULL AUTO_INCREMENT,
+  `telefone` varchar(9) DEFAULT NULL,
+  `nome` varchar(20) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `observacao` varchar(50) DEFAULT NULL,
+  `bairro` varchar(20) DEFAULT NULL,
+  `id_navios` int(11) DEFAULT NULL,
+  `dt` date DEFAULT NULL,
+  `hora` varchar(10) DEFAULT NULL,
+  `id_pacotes` int(11) DEFAULT NULL,
+  `valor` decimal(10,0) DEFAULT NULL,
+  `id_pgto` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_venda`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE `vendas_bkp` (
+  `id_venda` int(11) NOT NULL AUTO_INCREMENT,
+  `telefone` varchar(9) DEFAULT NULL,
+  `nome` varchar(20) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `observacao` varchar(50) DEFAULT NULL,
+  `bairro` varchar(20) DEFAULT NULL,
+  `id_navios` int(11) DEFAULT NULL,
+  `dt` date DEFAULT NULL,
+  `hora` varchar(10) DEFAULT NULL,
+  `id_pacotes` int(11) DEFAULT NULL,
+  `valor` decimal(10,0) DEFAULT NULL,
+  `id_pgto` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_venda`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+INSERT INTO `usuarios` VALUES (1,'admin','123','administrador');
